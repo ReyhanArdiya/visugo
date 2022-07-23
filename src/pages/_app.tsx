e8080220c54716f0d9a0169974e129b7614a5a37
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-07-23 23:05:00 +0700

    Setup `vitest`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
+// import "../styles/globals.css";
 import theme from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
