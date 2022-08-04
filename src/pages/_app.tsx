3d51bc77ae1300c48db3defb585e94dd06dcb3f3
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-08-04 16:38:23 +0700

    Create `getStorageClient`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider, Text } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 // import "../styles/globals.css";
 import theme from "../theme";
@@ -7,12 +7,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ChakraProvider theme={theme}>
             <Component {...pageProps} />
-            <Text
-                bg="blue.100"
-                color="red.100"
-            >
-                Meow
-            </Text>
         </ChakraProvider>
     );
 }
