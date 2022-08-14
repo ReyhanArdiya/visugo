6600f2aaf891e47631dc821489da568aa9a6074f
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-08-14 10:45:10 +0700

    Style `Button`

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,14 +1,17 @@
 import { extendTheme } from "@chakra-ui/react";
 import colors from "./colors";
+import Button from "./components/Button";
 import fonts from "./fonts";
 
 const theme = extendTheme({
     config: {
         useSystemColorMode: true,
     },
+    components: {
+        Button,
+    },
     colors,
     fonts,
 });
 
 export default theme;
-
