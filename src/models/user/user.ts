b6ea8a770a3791b83f7c164c7d639fbf1e42bbde
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-09-10 22:03:11 +0700

    Implement `UserDoc` `cart`

--- src/models/user/user.ts
+++ src/models/user/user.ts
@@ -1,13 +1,75 @@
 import { UserCredential } from "firebase/auth";
-import { doc, FirestoreDataConverter, setDoc } from "firebase/firestore";
+import {
+    doc,
+    DocumentReference,
+    Firestore,
+    FirestoreDataConverter,
+    setDoc,
+} from "firebase/firestore";
 import createPathSegments from "../../utils/firebase/client/firestore/create-path-segments";
 import { FirestoreCollection } from "../base/firestore-collection";
+import { ListingDoc } from "./listing";
+
+export const userDocConverter: FirestoreDataConverter<UserDoc> = {
+    fromFirestore(snapshot) {
+        const userDoc = snapshot.data() as Pick<UserDoc, "uid" | "cart">;
+
+        return new UserDoc(userDoc.uid, userDoc.cart);
+    },
+
+    toFirestore(modelObject) {
+        return { ...modelObject };
+    },
+};
+
+export interface CartItem {
+    ref: DocumentReference<ListingDoc>;
+    quantity: number;
+}
+
+export type Cart = Record<string, CartItem>;
 
 export class UserDoc {
     constructor(
         // CMT displayname comes from usercredential and photourl too
-        public readonly uid: UserCredential["user"]["uid"]
+        public readonly uid: UserCredential["user"]["uid"],
+        public cart: Cart = {}
     ) {}
+
+    public async cartUpdated(
+        db: Firestore,
+        listingDoc: DocumentReference<ListingDoc>,
+        incQuant: number | false
+    ) {
+        const userCol = new UserCollection(userDocConverter, db);
+        const userDoc = await userCol.getDocById(this.uid);
+        const cart = { ...userDoc.data()?.cart };
+
+        const deleteCartItem = () => delete cart[listingDoc.id];
+
+        if (cart) {
+            const item = cart[listingDoc.id];
+
+            if (incQuant === false) {
+                deleteCartItem();
+            } else if (!item) {
+                cart[listingDoc.id] = {
+                    quantity: incQuant,
+                    ref: listingDoc,
+                };
+            } else {
+                item.quantity += incQuant;
+
+                if (item.quantity <= 0) {
+                    deleteCartItem();
+                }
+            }
+
+            await userCol.updateDocById(userDoc.id, {
+                cart,
+            });
+        }
+    }
 
     // XXX Causes circural dependency!!!
     // listings() {
@@ -19,36 +81,30 @@ export class UserDoc {
     // }
 }
 
-export const userDocConverter: FirestoreDataConverter<UserDoc> = {
-    fromFirestore(snapshot) {
-        const userDoc = snapshot.data() as Pick<UserDoc, "uid">;
-
-        return new UserDoc(userDoc.uid);
-    },
-
-    toFirestore(modelObject) {
-        return { ...modelObject };
-    },
-};
-
-// CMT We don't constrain the T since each subcollection will have their own docs
+// @ts-expect-error: We don't constrain the T since each subcollection will have their own docs
 export class UserCollection<T = UserDoc> extends FirestoreCollection<T> {
     public collectionId = "users";
 
     constructor(
+        // @ts-expect-error: We don't constrain the T since each subcollection will have their own docs
         ...superParams: ConstructorParameters<typeof FirestoreCollection<T>>
     ) {
         const pathSegments = createPathSegments(["users"], superParams[2]);
 
+        // @ts-expect-error: We don't constrain the T since each subcollection will have their own docs
         super(superParams[0], superParams[1], pathSegments);
     }
 
     /**
      * Prefer using this over {@link UserCollection.add}.
      */
-    public async signUp(userId: string, data: T) {
+    public async signUp(
+        userId: string,
+        data: Pick<UserDoc, "cart" | "uid"> = { cart: {}, uid: userId }
+    ) {
         const userRef = doc(this.ref, userId);
 
+        // @ts-expect-error: Too tired babe
         await setDoc(userRef, data);
 
         return userRef;
